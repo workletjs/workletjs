b1e277e2cf684f4306b7695c79ae4e212833e725
ssping <jonnytoshen@gmail.com>
2025-10-16 18:37:16 +0800

    docs: add Sources category for ng-doc integration

---
+++ packages/ngx-openlayers/source/ng-doc.category.ts
@@ -0,0 +1,10 @@
+import { NgDocCategory } from '@ng-doc/core';
+
+const SourcesCategory: NgDocCategory = {
+  title: 'Sources',
+  route: 'sources',
+  expanded: true,
+  order: 3,
+};
+
+export default SourcesCategory;
