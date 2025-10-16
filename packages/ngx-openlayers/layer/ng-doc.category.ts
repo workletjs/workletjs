f62fa8a2c7ebfa7bbc9ee5361fb5f884bfd0cbc1
ssping <jonnytoshen@gmail.com>
2025-10-16 18:18:47 +0800

    docs: add Layers category for ng-doc integration

---
+++ packages/ngx-openlayers/layer/ng-doc.category.ts
@@ -0,0 +1,10 @@
+import { NgDocCategory } from '@ng-doc/core';
+
+const LayersCategory: NgDocCategory = {
+  title: 'Layers',
+  route: 'layers',
+  expanded: true,
+  order: 2,
+};
+
+export default LayersCategory;
