bfa8206b45232937b719d04ab7bfdc7c9100cde2
ssping <jonnytoshen@gmail.com>
2025-10-16 18:09:15 +0800

    docs: add GeneralCategory for ng-doc integration

---
+++ packages/ngx-openlayers/ng-doc.category.ts
@@ -0,0 +1,10 @@
+import { NgDocCategory } from '@ng-doc/core';
+
+const GeneralCategory: NgDocCategory = {
+  title: 'General',
+  route: 'general',
+  expanded: true,
+  order: 1,
+};
+
+export default GeneralCategory;
