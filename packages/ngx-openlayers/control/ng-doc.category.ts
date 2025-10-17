3752af399baa5820ab9c31382ea6c9632cfe10ab
ssping <jonnytoshen@gmail.com>
2025-10-17 09:20:08 +0800

    docs: add ControlsCategory for documentation navigation

---
+++ packages/ngx-openlayers/control/ng-doc.category.ts
@@ -0,0 +1,10 @@
+import { NgDocCategory } from '@ng-doc/core';
+
+const ControlsCategory: NgDocCategory = {
+  title: 'Controls',
+  route: 'controls',
+  expanded: true,
+  order: 4,
+};
+
+export default ControlsCategory;
