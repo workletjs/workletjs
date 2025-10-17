1928cf5ccd33e88f36dd7869c9006c3275cc9783
ssping <jonnytoshen@gmail.com>
2025-10-17 12:51:08 +0800

    feat(module:tile-source): enhance useTileSourceHostRef to support raster sources

--- packages/ngx-openlayers/source/tile/use-tile-source-host-ref.ts
+++ packages/ngx-openlayers/source/tile/use-tile-source-host-ref.ts
@@ -1,23 +1,43 @@
 import { inject, InjectOptions } from '@angular/core';
 import { WolTileLayerComponent } from '@workletjs/ngx-openlayers/layer/tile';
+import { WolRasterSourceComponent } from '@workletjs/ngx-openlayers/source/raster';
 import TileLayer from 'ol/layer/Tile';
+import RasterSource from 'ol/source/Raster';
 import TileSource from 'ol/source/Tile';
 
+export type DisposeRef = () => void;
+
 export interface TileSourceHostRef<T extends TileSource> {
-  setSource(source: T | null): void;
-  getInstance(): TileLayer | undefined;
+  setSource(source: T): DisposeRef;
+  getInstance(): TileLayer | RasterSource | undefined;
 }
 
 export function useTileSourceHostRef<T extends TileSource>(
   sourceName: string,
 ): TileSourceHostRef<T> {
   const options: InjectOptions = { host: true, optional: true };
+  const rasterSource = inject(WolRasterSourceComponent, options);
   const tileLayerHost = inject(WolTileLayerComponent, options);
+
+  if (rasterSource) {
+    return {
+      setSource: (source) => {
+        rasterSource.addSource(source);
+        return () => {
+          rasterSource.removeSource(source);
+        };
+      },
+      getInstance: () => rasterSource.getInstance(),
+    };
+  }
 
   if (tileLayerHost) {
     return {
       setSource: (source) => {
         tileLayerHost.getInstance()?.setSource(source);
+        return () => {
+          tileLayerHost.getInstance()?.setSource(null);
+        };
       },
       getInstance: () => tileLayerHost.getInstance(),
     };
