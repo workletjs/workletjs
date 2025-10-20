f538f2f72e0041b167b1610f276346f2b313c368
ssping <jonnytoshen@gmail.com>
2025-10-20 14:52:07 +0800

    feat(module:tile-source): enhance TileSourceHostRef to support DataTileSource and WebGLTileLayer

--- packages/ngx-openlayers/source/tile/use-tile-source-host-ref.ts
+++ packages/ngx-openlayers/source/tile/use-tile-source-host-ref.ts
@@ -1,23 +1,27 @@
 import { inject, InjectOptions } from '@angular/core';
 import { WolTileLayerComponent } from '@workletjs/ngx-openlayers/layer/tile';
+import { WolWebGLTileLayerComponent } from '@workletjs/ngx-openlayers/layer/webgl-tile';
 import { WolRasterSourceComponent } from '@workletjs/ngx-openlayers/source/raster';
 import TileLayer from 'ol/layer/Tile';
+import WebGLTileLayer from 'ol/layer/WebGLTile';
+import DataTileSource from 'ol/source/DataTile';
 import RasterSource from 'ol/source/Raster';
 import TileSource from 'ol/source/Tile';
 
 export type DisposeRef = () => void;
 
-export interface TileSourceHostRef<T extends TileSource> {
+export interface TileSourceHostRef<T extends TileSource | DataTileSource> {
   setSource(source: T): DisposeRef;
-  getInstance(): TileLayer | RasterSource | undefined;
+  getInstance(): TileLayer | WebGLTileLayer | RasterSource | undefined;
 }
 
-export function useTileSourceHostRef<T extends TileSource>(
+export function useTileSourceHostRef<T extends TileSource | DataTileSource>(
   sourceName: string,
 ): TileSourceHostRef<T> {
   const options: InjectOptions = { host: true, optional: true };
   const rasterSource = inject(WolRasterSourceComponent, options);
-  const tileLayerHost = inject(WolTileLayerComponent, options);
+  const webglTileLayer = inject(WolWebGLTileLayerComponent, options);
+  const tileLayer = inject(WolTileLayerComponent, options);
 
   if (rasterSource) {
     return {
@@ -31,19 +35,32 @@ export function useTileSourceHostRef<T extends TileSource>(
     };
   }
 
-  if (tileLayerHost) {
+  if (webglTileLayer) {
     return {
       setSource: (source) => {
-        tileLayerHost.getInstance()?.setSource(source);
+        webglTileLayer.getInstance()?.setSource(source as DataTileSource);
         return () => {
-          tileLayerHost.getInstance()?.setSource(null);
+          webglTileLayer.getInstance()?.setSource(null);
         };
       },
-      getInstance: () => tileLayerHost.getInstance(),
+      getInstance: () => webglTileLayer.getInstance(),
+    };
+  }
+
+  if (tileLayer) {
+    return {
+      setSource: (source) => {
+        tileLayer.getInstance()?.setSource(source);
+        return () => {
+          tileLayer.getInstance()?.setSource(null);
+        };
+      },
+      getInstance: () => tileLayer.getInstance(),
     };
   }
 
   throw new Error(
-    `No tile layer host found. Please wrap the ${sourceName} component in a TileLayer component.`,
+    `No TileSource host found. Please wrap the ${sourceName} component in a RasterSource, ` +
+      `WebGLTileLayer or TileLayer component.`,
   );
 }
