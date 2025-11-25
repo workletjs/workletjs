ed8e454f5687056a5fd73a3ada7ec313895ed9bf
沈同学 <jonnytoshen@gmail.com>
2025-11-25 11:23:48 +0800

    fix(module:utf-grid): ensure disposeRef is called on component destruction

--- packages/ngx-openlayers/source/utf-grid/utf-grid-source.component.ts
+++ packages/ngx-openlayers/source/utf-grid/utf-grid-source.component.ts
@@ -97,8 +97,12 @@ export class WolUTFGridSourceComponent implements OnChanges {
     });
 
     destroyRef.onDestroy(() => {
+      if (disposeRef) {
+        disposeRef();
+      }
+
       unByKey(Object.values(eventsKey));
-      disposeRef && disposeRef();
+
       this.instance = undefined;
     });
   }
