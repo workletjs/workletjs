1b07b26984c3234bf4b301b06f6f2828db2d842f
沈同学 <jonnytoshen@gmail.com>
2025-11-25 11:28:23 +0800

    fix(module:image-tile): ensure disposeRef is called on component destruction

--- packages/ngx-openlayers/source/image-tile/image-tile-source.component.ts
+++ packages/ngx-openlayers/source/image-tile/image-tile-source.component.ts
@@ -135,8 +135,12 @@ export class WolImageTileSourceComponent implements OnChanges {
 
     destroyRef.onDestroy(() => {
       if (this.instance) {
+        if (disposeRef) {
+          disposeRef();
+        }
+
         unByKey(Object.values(eventsKey));
-        disposeRef && disposeRef();
+        
         this.instance = undefined;
       }
     });
