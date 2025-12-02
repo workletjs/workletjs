6a7d2def1c98a015f842c03fe6c709ea4d01154b
Jonnytoshen <jonnytoshen@gmail.com>
2025-12-02 15:53:53 +0800

    fix(docs): update docsPath to point to the correct directory

--- apps/docs/ng-doc.config.ts
+++ apps/docs/ng-doc.config.ts
@@ -2,7 +2,7 @@ import { NgDocConfiguration } from '@ng-doc/builder';
 import { ngKeywordsLoader } from '@ng-doc/keywords-loaders';
 
 const config: NgDocConfiguration = {
-  docsPath: 'packages/ngx-openlayers',
+  docsPath: './',
   keywords: {
     loaders: [ngKeywordsLoader({})],
   },
