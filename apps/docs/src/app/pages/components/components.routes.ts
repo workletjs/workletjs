39a79df64f085dbf9290e5a3a35521909b8f130d
Jonnytoshen <jonnytoshen@gmail.com>
2025-12-02 15:58:34 +0800

    feat(docs): restructure components routing to include dynamic children routes

--- apps/docs/src/app/pages/components/components.routes.ts
+++ apps/docs/src/app/pages/components/components.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
+import { NG_DOC_ROUTING } from '@ng-doc/generated';
 import { ComponentsComponent } from './components.component';
+
+const COMPONENTS_CHILDREN_ROUTES =
+  NG_DOC_ROUTING.find((route) => route.title === 'Components')?.children ?? [];
 
 const routes: Routes = [
   {
     path: '',
     component: ComponentsComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'general/map',
+        pathMatch: 'full',
+      },
+      ...COMPONENTS_CHILDREN_ROUTES,
+    ],
   },
 ];
 
