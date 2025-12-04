4a1b0bb9ada91eadc183904dcc5cde7e099c440b
Jonnytoshen <jonnytoshen@gmail.com>
2025-12-04 17:25:59 +0800

    feat(module:vector-source): support VectorImageLayer

--- packages/ngx-openlayers/source/vector/use-vector-source-host-ref.ts
+++ packages/ngx-openlayers/source/vector/use-vector-source-host-ref.ts
@@ -1,14 +1,16 @@
 import { inject } from '@angular/core';
 import { WolHeatmapLayerComponent } from '@workletjs/ngx-openlayers/layer/heatmap';
+import { WolVectorImageLayerComponent } from '@workletjs/ngx-openlayers/layer/vector-image';
 import { FeatureLike } from 'ol/Feature';
 import Heatmap from 'ol/layer/Heatmap';
+import VectorImageLayer from 'ol/layer/VectorImage';
 import VectorSource from 'ol/source/Vector';
 
 export type DisposeRef = () => void;
 
 export interface VectorSourceHostRef<T extends VectorSource<FeatureLike>> {
   setSource(source: T): DisposeRef;
-  getInstance(): Heatmap<FeatureLike> | undefined;
+  getInstance(): Heatmap<FeatureLike> | VectorImageLayer | undefined;
 }
 
 export function useVectorSourceHostRef<T extends VectorSource<FeatureLike>>(
@@ -16,6 +18,7 @@ export function useVectorSourceHostRef<T extends VectorSource<FeatureLike>>(
 ): VectorSourceHostRef<T> {
   const options = { host: true, optional: true };
   const heatmapLayer = inject(WolHeatmapLayerComponent, options);
+  const vectorImageLayer = inject(WolVectorImageLayerComponent, options);
 
   if (heatmapLayer) {
     return {
@@ -27,10 +30,20 @@ export function useVectorSourceHostRef<T extends VectorSource<FeatureLike>>(
       },
       getInstance: () => heatmapLayer.getInstance(),
     };
+  } else if (vectorImageLayer) {
+    return {
+      setSource: (source) => {
+        vectorImageLayer.getInstance()?.setSource(source);
+        return () => {
+          vectorImageLayer.getInstance()?.setSource(null);
+        };
+      },
+      getInstance: () => vectorImageLayer.getInstance(),
+    };
   }
 
   throw new Error(
     `No VectorSource host found. Please wrap the ${sourceName} component in a ` +
-      `HeatmapLayer component.`,
+      `HeatmapLayer, VectorImageLayer component.`,
   );
 }
