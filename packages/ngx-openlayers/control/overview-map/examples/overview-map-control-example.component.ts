2db4d1afc28d622bfa9331038c6777f1e5732663
Jonnytoshen <jonnytoshen@gmail.com>
2025-12-08 10:14:06 +0800

    feat(module:overview-map): add example component for overview map control

---
+++ packages/ngx-openlayers/control/overview-map/examples/overview-map-control-example.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { WolMapModule } from '@workletjs/ngx-openlayers/map';
+import { WolViewModule } from '@workletjs/ngx-openlayers/view';
+import { WolOverviewMapControlModule } from '@workletjs/ngx-openlayers/control/overview-map';
+import { WolTileLayerModule } from '@workletjs/ngx-openlayers/layer/tile';
+import { WolOSMSourceModule } from '@workletjs/ngx-openlayers/source/osm';
+import { defaults } from 'ol/control/defaults';
+
+@Component({
+  selector: 'wol-overview-map-control-example',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    WolMapModule,
+    WolViewModule,
+    WolOverviewMapControlModule,
+    WolTileLayerModule,
+    WolOSMSourceModule,
+  ],
+  template: `
+    <wol-map class="h-96" [wolControls]="controls">
+      <wol-view [wolCenter]="[500000, 6000000]" [wolZoom]="7" />
+      <wol-overview-map-control>
+        <wol-tile-layer>
+          <wol-osm-source />
+        </wol-tile-layer>
+      </wol-overview-map-control>
+      <wol-tile-layer>
+        <wol-osm-source />
+      </wol-tile-layer>
+    </wol-map>
+  `,
+})
+export class WolOverviewMapControlExampleComponent {
+  readonly controls = defaults();
+}
