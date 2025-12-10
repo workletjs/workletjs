d23aa60427d39a636aba45a3a7d8295730111318
Jonnytoshen <jonnytoshen@gmail.com>
2025-12-10 14:15:44 +0800

    feat(module:interaction): add interaction module with public API and host reference

---
+++ packages/ngx-openlayers/interaction/interaction/use-interaction-host-ref.ts
@@ -0,0 +1,32 @@
+import { inject } from '@angular/core';
+import { WolMapComponent } from '@workletjs/ngx-openlayers/map';
+import Interaction from 'ol/interaction/Interaction';
+import Map from 'ol/Map';
+
+export interface InteractionHostRef<T extends Interaction> {
+  addInteraction(interaction: T): void;
+  removeInteraction(interaction: T): T | undefined;
+  getInstance(): Map | undefined;
+}
+
+export function useInteractionHostRef<T extends Interaction>(
+  interactionName: string,
+): InteractionHostRef<T> {
+  const mapRef = inject(WolMapComponent, { host: true, optional: true });
+
+  if (mapRef) {
+    return {
+      addInteraction: (interaction) => {
+        mapRef.getInstance()?.addInteraction(interaction);
+      },
+      removeInteraction: (interaction) => {
+        return mapRef.getInstance()?.removeInteraction(interaction) as T | undefined;
+      },
+      getInstance: () => mapRef.getInstance(),
+    };
+  }
+
+  throw new Error(
+    `No interaction host found. Please wrap the ${interactionName} interaction component in a Map component.`,
+  );
+}
