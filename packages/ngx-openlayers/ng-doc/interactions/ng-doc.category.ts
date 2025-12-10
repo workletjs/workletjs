3c5160fdb1d7b02416263a1e2ffc4009d0f69a94
Jonnytoshen <jonnytoshen@gmail.com>
2025-12-10 14:11:08 +0800

    docs(ng-doc): add InteractionsCategory for documentation structure

---
+++ packages/ngx-openlayers/ng-doc/interactions/ng-doc.category.ts
@@ -0,0 +1,12 @@
+import { NgDocCategory } from '@ng-doc/core';
+import ComponentsCategory from '../ng-doc.category';
+
+const InteractionsCategory: NgDocCategory = {
+  title: 'Interactions',
+  route: 'interactions',
+  category: ComponentsCategory,
+  expanded: false,
+  order: 5,
+};
+
+export default InteractionsCategory;
