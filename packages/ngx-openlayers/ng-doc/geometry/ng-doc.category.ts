1d51afa4823537db14aae1e5b6e95c3e095175f6
Jonnytoshen <jonnytoshen@gmail.com>
2025-12-24 17:09:57 +0800

    docs: add GeometryCategory for documentation

---
+++ packages/ngx-openlayers/ng-doc/geometry/ng-doc.category.ts
@@ -0,0 +1,12 @@
+import { NgDocCategory } from '@ng-doc/core';
+import ComponentsCategory from '../ng-doc.category';
+
+const GeometryCategory: NgDocCategory = {
+  title: 'Geometry',
+  route: 'geometry',
+  category: ComponentsCategory,
+  expanded: false,
+  order: 1,
+};
+
+export default GeometryCategory;
