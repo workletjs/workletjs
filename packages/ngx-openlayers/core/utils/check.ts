8e71bc10caf0ab129fbaeae4d505d9ddefa24d98
Jonnytoshen <jonnytoshen@gmail.com>
2025-12-24 17:08:50 +0800

    feat(module:utils): add isNotNil util

--- packages/ngx-openlayers/core/utils/check.ts
+++ packages/ngx-openlayers/core/utils/check.ts
@@ -9,3 +9,13 @@ import { WolSafeAny } from '@workletjs/ngx-openlayers/core/types';
 export function isNil(value: WolSafeAny): value is null | undefined {
   return value === null || value === undefined;
 }
+
+/**
+ * Checks if a value is not null or undefined.
+ *
+ * @param value The value to check.
+ * @returns True if the value is not null or undefined, false otherwise.
+ */
+export function isNotNil<T>(value: T | null | undefined): value is T {
+  return !isNil(value);
+}
