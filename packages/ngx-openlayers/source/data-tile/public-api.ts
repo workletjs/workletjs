418df00dfaae930c88e5c629e4b3447d3ce0e0bc
Jonnytoshen <jonnytoshen@gmail.com>
2026-02-11 13:51:32 +0800

    feat(module:data-tile-source): add supports for Flow layer component

--- packages/ngx-openlayers/source/data-tile/public-api.ts
+++ packages/ngx-openlayers/source/data-tile/public-api.ts
@@ -1,2 +1,3 @@
 export * from './data-tile-source.component';
 export * from './data-tile-source.module';
+export * from './use-data-tile-source-host-ref';
---
+++ packages/ngx-openlayers/source/data-tile/use-data-tile-source-host-ref.ts
@@ -0,0 +1,80 @@
+import { inject, InjectOptions } from '@angular/core';
+import { WolFlowLayerComponent } from '@workletjs/ngx-openlayers/layer/flow';
+import { WolTileLayerComponent } from '@workletjs/ngx-openlayers/layer/tile';
+import { WolWebGLTileLayerComponent } from '@workletjs/ngx-openlayers/layer/webgl-tile';
+import { WolRasterSourceComponent } from '@workletjs/ngx-openlayers/source/raster';
+import FlowLayer from 'ol/layer/Flow';
+import TileLayer from 'ol/layer/Tile';
+import WebGLTileLayer from 'ol/layer/WebGLTile';
+import DataTileSource from 'ol/source/DataTile';
+import RasterSource from 'ol/source/Raster';
+
+export type DisposeRef = () => void;
+
+export interface DataTileSourceHostRef<T extends DataTileSource> {
+  setSource(source: T): DisposeRef;
+  getInstance(): TileLayer | WebGLTileLayer | FlowLayer | RasterSource | undefined;
+}
+
+export function useDataTileSourceHostRef<T extends DataTileSource>(
+  sourceName: string,
+): DataTileSourceHostRef<T> {
+  const options: InjectOptions = { host: true, optional: true };
+  const rasterSource = inject(WolRasterSourceComponent, options);
+  const webglTileLayer = inject(WolWebGLTileLayerComponent, options);
+  const tileLayer = inject(WolTileLayerComponent, options);
+  const flowLayer = inject(WolFlowLayerComponent, options);
+
+  if (rasterSource) {
+    return {
+      setSource: (source) => {
+        rasterSource.addSource(source);
+        return () => {
+          rasterSource.removeSource(source);
+        };
+      },
+      getInstance: () => rasterSource.getInstance(),
+    };
+  }
+
+  if (webglTileLayer) {
+    return {
+      setSource: (source) => {
+        webglTileLayer.getInstance()?.setSource(source as DataTileSource);
+        return () => {
+          webglTileLayer.getInstance()?.setSource(null);
+        };
+      },
+      getInstance: () => webglTileLayer.getInstance(),
+    };
+  }
+
+  if (tileLayer) {
+    return {
+      setSource: (source) => {
+        tileLayer.getInstance()?.setSource(source);
+        return () => {
+          tileLayer.getInstance()?.setSource(null);
+        };
+      },
+      getInstance: () => tileLayer.getInstance(),
+    };
+  }
+
+  if (flowLayer) {
+    return {
+      setSource: (source) => {
+        flowLayer.getInstance()?.setSource(source as DataTileSource);
+        return () => {
+          flowLayer.getInstance()?.setSource(null);
+        };
+      },
+      getInstance: () => flowLayer.getInstance(),
+    };
+  }
+
+  throw new Error(
+    `No TileSource host found. Please wrap the ${sourceName} component in a RasterSource, ` +
+      `WebGLTileLayer, FlowLayer or TileLayer component.`,
+  );
+}
