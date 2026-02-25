f8ee15fca1bcc556c014c0f824218f756bf810ee
Jonnytoshen <jonnytoshen@gmail.com>
2026-02-25 14:50:37 +0800

    feat(module:vector-tile): enhance useVectorTileSourceHostRef to support WebGLVectorTileLayer

--- packages/ngx-openlayers/source/vector-tile/use-vector-tile-source-host-ref.ts
+++ packages/ngx-openlayers/source/vector-tile/use-vector-tile-source-host-ref.ts
@@ -2,15 +2,18 @@ import { InjectOptions, inject } from '@angular/core';
 
 import { FeatureLike } from 'ol/Feature';
 import VectorTileLayer from 'ol/layer/VectorTile';
+import WebGLVectorTileLayer from 'ol/layer/WebGLVectorTile';
+import RenderFeature from 'ol/render/Feature';
 import VectorTileSource from 'ol/source/VectorTile';
 
 import { WolVectorTileLayerComponent } from '@workletjs/ngx-openlayers/layer/vector-tile';
+import { WolWebGLVectorTileLayerComponent } from '@workletjs/ngx-openlayers/layer/webgl-vector-tile';
 
 export type DisposeRef = () => void;
 
 export interface VectorTileSourceHostRef<T extends VectorTileSource<FeatureLike>> {
   setSource(source: T): DisposeRef;
-  getInstance(): VectorTileLayer | undefined;
+  getInstance(): VectorTileLayer | WebGLVectorTileLayer | undefined;
 }
 
 export function useVectorTileSourceHostRef<T extends VectorTileSource<FeatureLike>>(
@@ -18,6 +21,7 @@ export function useVectorTileSourceHostRef<T extends VectorTileSource<FeatureLik
 ): VectorTileSourceHostRef<T> {
   const options: InjectOptions = { host: true, optional: true };
   const vectorTileLayer = inject(WolVectorTileLayerComponent, options);
+  const webGLVectorTileLayer = inject(WolWebGLVectorTileLayerComponent, options);
 
   if (vectorTileLayer) {
     return {
@@ -31,8 +35,20 @@ export function useVectorTileSourceHostRef<T extends VectorTileSource<FeatureLik
     };
   }
 
+  if (webGLVectorTileLayer) {
+    return {
+      setSource: (source) => {
+        webGLVectorTileLayer.getInstance()?.setSource(source as VectorTileSource<RenderFeature>);
+        return () => {
+          webGLVectorTileLayer.getInstance()?.setSource(null);
+        };
+      },
+      getInstance: () => webGLVectorTileLayer.getInstance(),
+    };
+  }
+
   throw new Error(
     `No VectorTileSource host found. Please wrap the ${sourceName} component in a ` +
-      `VectorTileLayer component.`,
+      `VectorTileLayer or WebGLVectorTileLayer component.`,
   );
 }
