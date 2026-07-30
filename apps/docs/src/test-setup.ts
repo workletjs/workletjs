cf365be2480fb4d31c762bfd74189d8cf2ec5e90
Jonnytoshen <jonnytoshen@gmail.com>
2026-07-30 14:56:06 +0800

    test(docs): polyfill ResizeObserver in test setup

--- apps/docs/src/test-setup.ts
+++ apps/docs/src/test-setup.ts
@@ -1,7 +1,13 @@
 import '@analogjs/vitest-angular/setup-zone';
+import ResizeObserverPolyfill from 'resize-observer-polyfill';
 
 import '@angular/compiler';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
 getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
+
+if (typeof ResizeObserver === 'undefined') {
+  // eslint-disable-next-line
+  (globalThis as any).ResizeObserver = ResizeObserverPolyfill;
+}
